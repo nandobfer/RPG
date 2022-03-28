3e78eccc0adadc169741fd78181853dd20b580a5
nandobfer <nandobfer@yahoo.com.br>
2022-03-28 02:13:03 -0300

    Added Button() methods

--- src/objects/Button.py
+++ src/objects/Button.py
@@ -3,26 +3,46 @@ import pygame, conf
 class Button():
     # text, size = (width, height), img = string ("path/file.png")
     def __init__(self, text, size, img):
-        self.text = conf.text.render(text, True, (255, 255, 255))
+        self.text = conf.text.render(text, True, (170, 170, 170))
+        self.text_mouse = conf.text.render(text, True, (255, 255, 255))
         self.size = size
         self.width = size[0]
         self.height = size[1]
         self.img = pygame.image.load(img).convert_alpha()
+        self.img = pygame.transform.scale(self.img, self.size)
         self.offset_x = 0
         self.offset_y = 0
+        self.x = 0
+        self.y = 0
+        self.position = (0,0)
+
+    # set size
+    def setSize(self, size):
+        self.size = size
+        self.width = size[0]
+        self.height = size[1]
 
     # set offset for alpha distances in images
     def setOffset(self, x, y):
         self.offset_x = x
         self.offset_y = y
 
+    # set position for easy access
+    def setPosition(self, x, y):
+        self.x = x
+        self.y = y
+        self.position = (x,y)
+
     # mouse = (x, y), position = (x, y)
-    def getEvent(self, mouse, position):
-        if position[0] <= mouse[0] <= position[0] + self.width and position[1] <= mouse[1] <= position[1] + self.height:
+    def getMouse(self, mouse):
+        if self.x - self.offset_x <= mouse[0] <= self.x - self.offset_x + self.width and self.y <= mouse[1] <= self.y - 2 * self.offset_y + self.height:
             return True
 
-    # screen, position = (x, y)
-    def draw(self, screen, position):
-        screen.blit(self.img, (position[0] - self.offset_x, position[1] - self.offset_y))
-        screen.blit(self.text, (position[0] + 100, position[1] + 15))
+    # screen, mouse = (x,y)
+    def draw(self, screen, mouse):
+        screen.blit(self.img, (self.x - self.offset_x, self.y - self.offset_y))
+        if self.getMouse(mouse):
+            screen.blit(self.text_mouse, (self.x + 100, self.y + 15))
+        else:
+            screen.blit(self.text, (self.x + 100, self.y + 15))
 
--- src/screens/start_menu.py
+++ src/screens/start_menu.py
@@ -11,30 +11,53 @@ background = pygame.transform.scale(background, init.resolution)
 import conf
 
 def start():
+    # Start Menu Flag
     start_menu = True
+
+    # System Clock
     clock = pygame.time.Clock()
-    quit = Button("Quit", (100,30), "assets/buttons/button.png")
+
+    # Quit Button
+    quit = Button("Quit", (300,200), "assets/buttons/button.png")
     quit.setOffset(10, 60)
+    quit.setPosition(710, 680)
+
+    # Game Loop
     while start_menu:
+
+        # Getting mouse position = (x,y)
         mouse = pygame.mouse.get_pos()
+
+        # Refresh Rate - FPS
         clock.tick(10)
+
+        # Show Background
         screen.blit(background, (0, 0))
 
+        # Start listening for events
         for event in pygame.event.get():
+
+            # Closing game window
             if event.type == pygame.QUIT:
                 quitGame()
 
+            # On mouse click
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                # Quit Button
+                if quit.getMouse(mouse):
+                    quitGame()
 
-        quit.draw(screen, (710, 680))
+        # Draw Buttons
+        quit.draw(screen, mouse)
+
+        # Update Screen
         pygame.display.update()
+
+        # Print mouse coordenates in console
         print(mouse)
 
 def quitGame():
     sys.exit()
 
-# Calling start_menu
-# start()
 
 
