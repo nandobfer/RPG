5d4b6ad629ed4225e54b44de9a1bc2ee180be241
nandobfer <nandobfer@yahoo.com.br>
2022-03-28 00:21:56 -0300

    Changed start background

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys, screens.start_menu, init, conf, objects.Creature
 from src.objects.Creature import Creature
 import src.screens.start_menu
 
--- src/screens/start_menu.py
+++ src/screens/start_menu.py
@@ -4,12 +4,13 @@ from pygame import mixer
 
 # create a screen (width,height)
 screen = pygame.display.set_mode(init.resolution, DOUBLEBUF, 16)
+import conf
 
 def startMenu():
     start_menu = True
     while start_menu:
         screen.fill((25,25,150))
-        pygame.display.update()
+        screen.blit(conf.background, (0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +18,9 @@ def startMenu():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
+
+        pygame.display.update()
+
 
 def quit():
     sys.exit()
